b3698aa039143ba0dcdab153cb0421db6dce0c40
whipspiders <phrynidae@gmail.com>
2025-03-28 21:51:51 +0500

    Mushroom trees & physics based movement & primitive interaction system

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    private Interactable currentInteractable;
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        Interactable interactable = other.GetComponent<Interactable>();
+        if (interactable != null)
+        {
+            currentInteractable = interactable;
+            Debug.Log("It's interactable");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Interactable interactable = other.GetComponent<Interactable>();
+        if (interactable == null)
+        {
+            if (interactable == currentInteractable) 
+            {
+                currentInteractable = null;
+            }
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            currentInteractable.Interact();
+        }
+    }
+}
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,43 +4,46 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    private Rigidbody2D rigidbody;
     public float speed;
     Animator animComponent;
+    
     // Start is called before the first frame update
     // Update is called once per frame
     void Start()
     {
-            animComponent = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        animComponent = GetComponent<Animator>();
     }
     void Update()
     {   
-        animComponent.SetBool("WalkLeft", false);
-        animComponent.SetBool("WalkDown", false);
-        animComponent.SetBool("WalkRight", false);
-        animComponent.SetBool("WalkUp", false);
         Vector3 moveVector = Vector3.zero;
         if (Input.GetKey(KeyCode.W)) 
         {
             moveVector += new Vector3(0, 1, 0);
             animComponent.SetBool("WalkUp", true);
         }
+        else {animComponent.SetBool("WalkUp", false);}
         if (Input.GetKey(KeyCode.A)) 
         {
             moveVector += new Vector3(-1, 0, 0);
             animComponent.SetBool("WalkLeft", true);
         }
+        else {animComponent.SetBool("WalkLeft", false);}
         if (Input.GetKey(KeyCode.S)) 
         {
             moveVector += new Vector3(0, -1, 0);
             animComponent.SetBool("WalkDown", true);
         }
+        else {animComponent.SetBool("WalkDown", false);}
         if (Input.GetKey(KeyCode.D)) 
         {
             moveVector += new Vector3(1, 0, 0);
             animComponent.SetBool("WalkRight", true);
         }
+        else {animComponent.SetBool("WalkRight", false);}
         moveVector.Normalize();
-        transform.position = transform.position + moveVector * speed * Time.deltaTime;
+        rigidbody.velocity = moveVector * speed;
 
 
         ///animation player
